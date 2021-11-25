d8a4316d2f026c840317c8f86f43d020d41eb9b2
Lupus590 <lupussolitarius590@gmail.com>
2021-11-25 18:43:07 +0000

    fix shell api not appearing

--- Turtle UI Code/CC Side/repl.lua
+++ Turtle UI Code/CC Side/repl.lua
@@ -1,4 +1,5 @@
 local ws = http.websocket("ws://localhost:4000/test")
+-- TODO: make this easier to read
 
 local function main()
     while true do
@@ -6,7 +7,9 @@ local function main()
         if commandAsJsonString then
             local commandData = textutils.unserialiseJSON(commandAsJsonString)
             if commandData and commandData.computerID == os.getComputerID() and commandData.type == "command" then
-                local func, err = loadstring("return "..commandData.command) -- BUG: missing shell API
+                local env = setmetatable({}, { __index = _ENV }) -- inherit our env, posibly not a good idea
+                env._ENV = env
+                local func, err = load("return "..commandData.command, nil,  nil, env)
                 if func then
                     local returns = table.pack(pcall(func))
                     returns.n = nil
