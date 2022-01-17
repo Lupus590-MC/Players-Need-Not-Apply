224525c58ceab103729010341be70649b94d7c55
Lupus590 <lupussolitarius590@gmail.com>
2022-01-17 05:12:29 +0000

    basic tabs working

--- Web Dashboard/script.js
+++ Web Dashboard/script.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function(){
     var $ = jQuery;
+
+    var tabs = $( "#tabs" );
+    tabs.tabs();
+    var newTabButton = $("#new-tab");
+    var tabNumber = 3;
+    newTabButton.click(function(){
+        tabNumber+=1;
+        tabs.children("ul").append("<li><a href=\"#tabs-"+tabNumber+"\">Aenean lacinia</a></li>");
+        tabs.append("<div id=\"tabs-"+tabNumber+"\">"+"<p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>"+"<p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>"+"</div>");
+        tabs.tabs("refresh");
+    });
+
+
     var targetIdField = $("#targetId");
     var commandField = $("#command");
     var submitButton = $("#submit");
@@ -13,7 +26,7 @@ jQuery(document).ready(function(){
 
 
     var ws = new WebSocket("ws://localhost:4000/"+webSocketName);
-    ws.onmessage = function (evt) { 
+    ws.onmessage = function (evt) {
         var received_msg = JSON.parse(evt.data);
         //console.log(received_msg);
 
@@ -39,7 +52,7 @@ jQuery(document).ready(function(){
          var targetId = parseInt(targetIdField.val());
          var command = commandField.val();
          var commandPacket = { type: "command", command: command, computerID: targetId};
-         
+
          responceField.val("");
          errorField.val("");
 
