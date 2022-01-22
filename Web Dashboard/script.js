e6f1b51f70f65a339ae43678aab564e4f28854fd
Lupus590 <lupussolitarius590@gmail.com>
2022-01-22 05:24:32 +0000

    switch to let

--- Web Dashboard/script.js
+++ Web Dashboard/script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
-    var $ = jQuery;
+    let $ = jQuery;
 
-    var tabs = $("#tabs");
+    let tabs = $("#tabs");
     tabs.tabs();
 
     /**
@@ -17,39 +17,39 @@ jQuery(document).ready(function(){
         }
 
         tabs.children("ul").append("<li><a href=\"#computer-"+ccComputerID+"\">"+ccComputerLabel+"#"+ccComputerID+"</a></li>");
-        var clonedTemplate = $("#template").clone();
+        let clonedTemplate = $("#template").clone();
         clonedTemplate.attr("id", "computer-"+ccComputerID);
         clonedTemplate.appendTo(tabs);
         tabs.tabs("refresh");
-    } 
+    }
 
+    let urlParams = new URLSearchParams(window.location.search);
+    let webSocketName = urlParams.get('ws') || "test";
+
+    //TODO: maybe use alert boxes for when the REPL errors?
     //TODO: rewrite to handle the tab system
-    var targetIdField = $("#targetId");
-    var commandField = $("#command");
-    var submitButton = $("#submit");
-    var responceField = $("#responce");
-    var errorField = $("#error");
-    var statusFields = {};
-
-    var urlParams = new URLSearchParams(window.location.search);
-    var webSocketName = urlParams.get('ws') || "test";
-    //console.log(webSocketName);
+    let targetIdField = $("#targetId");
+    let commandField = $("#command");
+    let submitButton = $("#submit");
+    let responceField = $("#responce");
+    let errorField = $("#error");
+    let statusFields = {};
 
 
-    var ws = new WebSocket("ws://localhost:4000/"+webSocketName);
+    let ws = new WebSocket("ws://localhost:4000/"+webSocketName);
     ws.onmessage = function (evt) {
-        var received_msg = JSON.parse(evt.data);
+        let received_msg = JSON.parse(evt.data);
         //console.log(received_msg);
 
         responceField.val("");
         errorField.val("");
 
         if(received_msg.type == "commandResponce"){
-            var responceString = JSON.stringify(received_msg.responce);
+            let responceString = JSON.stringify(received_msg.responce);
             responceField.val(responceString);
         }
         else if(received_msg.type == "error"){
-            var errorMessageString = JSON.stringify(received_msg.errorMessage);
+            let errorMessageString = JSON.stringify(received_msg.errorMessage);
             errorField.val(errorMessageString);
         }
         else{
@@ -60,9 +60,9 @@ jQuery(document).ready(function(){
 
     // TODO: rewrite https://stackoverflow.com/questions/17451660/one-click-event-handler-for-multiple-buttons
     submitButton.click(function(){
-        var targetId = parseInt(targetIdField.val());
-        var command = commandField.val();
-        var commandPacket = { type: "command", command: command, computerID: targetId};
+        let targetId = parseInt(targetIdField.val());
+        let command = commandField.val();
+        let commandPacket = { type: "command", command: command, computerID: targetId};
 
         responceField.val("");
         errorField.val("");
@@ -74,8 +74,8 @@ jQuery(document).ready(function(){
 
     // DEBUG STUFF
 
-    var newTabButton = $("#new-tab");
-    var tabNumber = 0;
+    let newTabButton = $("#new-tab");
+    let tabNumber = 0;
     newTabButton.click(function(){
         tabNumber+=1;
         onNewConnection(tabNumber);
@@ -83,7 +83,7 @@ jQuery(document).ready(function(){
 
 
     //why did I add ways to remove tabs, disconnects are probably only a sign that something is wrong?
-    var removeTabButton = $("#remove-tab");
+    let removeTabButton = $("#remove-tab");
     removeTabButton.click(function(){
         if(tabNumber>0){
             tabNumber-=1;
@@ -93,8 +93,8 @@ jQuery(document).ready(function(){
         }
     });
 
-    var removeSpecificTabButton = $("#remove-specific-tab");
-    var specificTabToRemove = $("#specific-tab-to-remove");
+    let removeSpecificTabButton = $("#remove-specific-tab");
+    let specificTabToRemove = $("#specific-tab-to-remove");
     removeSpecificTabButton.click(function(){
         if(tabNumber>0 && specificTabToRemove.val() !== ""){
             tabs.children("ul").children().children().each(
