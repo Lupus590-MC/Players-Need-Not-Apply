ae3dbcf11deb8da948c060b21f9ace7f8f973e4f
Lupus590 <lupussolitarius590@gmail.com>
2022-03-07 02:07:29 +0000

    I think this looks a bit cleaner
    and notes

--- Mod Pack/datapack/data/computercraft/lua/rom/programs/websocket_repl.lua
+++ Mod Pack/datapack/data/computercraft/lua/rom/programs/websocket_repl.lua
@@ -6,7 +6,7 @@ if not ws then
 	error(err, 0)
 end
 -- TODO: make this easier to read
--- TODO: don't assume that ws is seccure, verify that message is from control center or toher trusted source - https://gist.github.com/MCJack123/7752c85918bcf23ada028abd615e8750
+-- TODO: don't assume that ws is secure, verify that message is from control center or other trusted source - https://gist.github.com/MCJack123/7752c85918bcf23ada028abd615e8750
 -- TODO: commands can be dropped if the computer is running a command already and happens to pull an event
 
 local function soundOff()
@@ -14,17 +14,23 @@ local function soundOff()
 	ws.send(errAsJsonString)
 end
 
-local function runCommand(commandData)
+local function runCommandAndSendResponce(commandData)
+	if commandData.command == "exit()" then
+		local returnsAsJsonString = textutils.serialiseJSON({ type = "commandResponse", response = "exiting ws REPL", computerId = os.getComputerID() })
+		ws.send(returnsAsJsonString)
+		return
+	end
+
 	local env = setmetatable({}, { __index = _ENV }) -- inherit our env, possibly not a good idea
 	env._ENV = env
-	local func, err = load("return "..commandData.command, nil,  nil, env)
+	local func, err = load("return "..commandData.command, nil,  nil, env) -- TODO: we don't handle multiple commands very well, might be worth looking at how the lua REPL works
 	if func then
 		local returns = table.pack(pcall(func))
 		returns.n = nil
 		local returnsAsJsonString = textutils.serialiseJSON({ type = "commandResponse", response = returns, computerId = os.getComputerID() })
 		ws.send(returnsAsJsonString)
 	else
-		local errAsJsonString = textutils.serialiseJSON({ type = "error", errorInfo = err, computerId = os.getComputerID(), fatal = false })
+		local errAsJsonString = textutils.serialiseJSON({ type = "error", errorInfo = err, computerId = os.getComputerID(), fatal = false }) -- TODO: commandNotUnderstood packet?
 		ws.send(errAsJsonString)
 	end
 end
@@ -39,13 +45,7 @@ local function main()
         if commandAsJsonString then
             local commandData = textutils.unserialiseJSON(commandAsJsonString)
             if commandData and commandData.computerId == os.getComputerID() and commandData.type == "command" then
-				if commandData.command == "exit()" then
-					local returnsAsJsonString = textutils.serialiseJSON({ type = "commandResponse", response = "exiting ws REPL", computerId = os.getComputerID() })
-					ws.send(returnsAsJsonString)
-					return
-				else
-                	runCommand(commandData)
-				end
+                runCommandAndSendResponce(commandData.command)
 			elseif commandData and commandData.type == "soundOff" then
 				soundOff()
 				print("sounding off")
