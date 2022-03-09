e6f31440b1c67548d1c60414ec7a20117c169a04
Lupus590 <lupussolitarius590@gmail.com>
2022-03-09 23:47:13 +0000

    overworld bot working

--- New Player Setup Backend/src/main/java/lupus590/players_need_not_apply/BotInterface.java
+++ New Player Setup Backend/src/main/java/lupus590/players_need_not_apply/BotInterface.java
@@ -3,29 +3,62 @@ package lupus590.players_need_not_apply;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
+import java.io.IOException;
+import java.util.UUID;
+
 
 // TODO: discord bot
 public class BotInterface {
+    private final String rootConnectionUrl;
+
+    private final ITurtleSpawner overworldTurtleSpawner;
+    private final ITurtleSpawner netherTurtleSpawner;
+    private final ITurtleSpawner endTurtleSpawner;
+
+    private final boolean requireOfferingsForOtherWorld;
+
     public BotInterface(String rootConnectionUrl, ITurtleSpawner overworldTurtleSpawner, ITurtleSpawner netherTurtleSpawner, ITurtleSpawner endTurtleSpawner, boolean requireOfferingsForOtherWorld) {
-        //TODO: use args
+        this.overworldTurtleSpawner = overworldTurtleSpawner;
+        this.netherTurtleSpawner = netherTurtleSpawner;
+        this.endTurtleSpawner = endTurtleSpawner;
+        this.rootConnectionUrl = rootConnectionUrl;
+        this.requireOfferingsForOtherWorld = requireOfferingsForOtherWorld;
+    }
+
+    private UUID spawnTurtle(String world, Coords coords, UUID connectionUUID) throws IOException, InterruptedException {
+        if (world.equalsIgnoreCase("o")){
+            connectionUUID = overworldTurtleSpawner.spawnTurtle(true);
+        } else if (world.equalsIgnoreCase("n")){
+            connectionUUID = netherTurtleSpawner.spawnTurtle(connectionUUID, coords,true);
+        } else if (world.equalsIgnoreCase("e")) {
+            connectionUUID = endTurtleSpawner.spawnTurtle(connectionUUID, coords,true);
+        } else {
+            throw new RuntimeException("Unknown world.");
+        }
+        return connectionUUID;
     }
 
     public void run() {
-        // Insert your bot's token here
         String token = ""; // TODO: load from file or command line
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-        // Add a listener which answers with "Pong!" if someone writes "!ping"
-            api.addMessageCreateListener(event ->
-
-        {
-            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Pong!");
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!create overworld")) {
+                UUID connectionUUID = null;
+                try {
+                    connectionUUID = spawnTurtle("o", null, null);
+                }
+                catch (IOException | InterruptedException e) {
+                    event.getChannel().sendMessage("An exception was caught while trying to make your turtle.");
+                    e.printStackTrace();
+                    return;
+                }
+                event.getChannel().sendMessage("Your connection URL: "+rootConnectionUrl+connectionUUID);
             }
         });
 
         // Print the invite url of your bot
-            System.out.println("You can invite the bot by using the following url: "+api.createBotInvite());
+        System.out.println("You can invite the bot by using the following url: "+api.createBotInvite());
     }
 }
